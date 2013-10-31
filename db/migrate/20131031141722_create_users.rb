6fce0e8e7c4f2c70c00f67781f067f325b643820
Mohammed Ameen <EMAIL>
2013-10-31 19:48:20 +0530

    Added user

---
+++ db/migrate/20131031141722_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.string :phone
+
+      t.timestamps
+    end
+    add_index :users, :email
+  end
+end
