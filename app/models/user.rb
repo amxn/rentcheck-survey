c56f21dfd5928f7be206d176ac6878407444dcea
Mohammed Ameen <ameen.in@gmail.com>
2013-11-01 12:34:19 +0530

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :address, :city, :email, :name, :password, :phone, :state, :zipcode
+
+  has_one :survey
+
+  has_many :responses
+  has_many :questions, through: :responses
 end
